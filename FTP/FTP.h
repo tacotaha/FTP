3ab77e340157ce119b9ca291a80fc9db5fae6189
Taha Azzaoui <tazzaoui@cs.uml.edu>
2018-05-02 21:10:34 -0400

    Added FTP Common Header

--- FTP/FTP.h
+++ FTP/FTP.h
@@ -1,12 +1,18 @@
 #ifndef __FTP_H__
 #define __FTP_H__
 
+#include <stdio.h>
 #include "../Stream/Stream.h"
 
-static const char* welcome =  "-=(<*>)=-.:. (( Welcome to The FTP server)) .:.-=(<*>)=-";
+#define CMD_LEN 5
+#define ARG_LEN BUF
+#define MSG_LEN CMD_LEN + ARG_LEN
+
+//static const char* welcome =  "-=(<*>)=-.:. (( Welcome to The FTP server)) .:.-=(<*>)=-";
+
 typedef struct command{
-  char cmd[5];
-  char arg[BUF];
+  char cmd[CMD_LEN];
+  char arg[ARG_LEN];
 }Command;
 
 /* 
@@ -23,12 +29,33 @@ int get_response(char* buffer, int sockfd, int print);
 /*
   Send a valid FTP command to the server.
   
-  @param command : A null-terminated string containing the command.
+  @param command : A nonempty command struct
   @param sockfd  : The socket file descriptor associated with the server.
   @return int    : The number of bytes sent to the server, or a int < 0 
                    if there occured an error.
  */
 int send_command(const Command* command, int sockfd);
+
+
+/*
+  Read and convert an FTP command from a file (usually stdin)
+  
+  @param command : A command struct
+  @param sockfd  : The socket file descriptor associated with the server.
+  @return int    : The number of bytes read from the file (max is BUF2)
+ */
+int read_command(Command* command, FILE* fp);
+
+/*
+  Send a valid FTP command to the server.
+  
+  @param command : A nonempty command struct.
+  @param sockfd  : The socket file descriptor associated with the server.
+  @param print   : A boolean value specifying wheather or not to print the response.
+  @return int    : The number of bytes sent to the server, or a int < 0 
+                   if there occured an error.
+ */
+int get_command(Command* command, int sockfd, int print);
 
 /* Server Functions for handling each command each have the format...
    @param arg    : The null-terminated paramater of the command, usually
@@ -38,20 +65,7 @@ int send_command(const Command* command, int sockfd);
                    if there occured an error.
 */
 
+
 int handle_pwd(const char* arg, int sockfd);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 #endif
