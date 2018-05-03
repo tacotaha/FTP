1d3ceabd4efb71ebd92c8defa8d4bab28e5456d3
Taha Azzaoui <tazzaoui@cs.uml.edu>
2018-05-03 02:02:26 -0400

    Updated FTP functionality

--- FTP/FTP.c
+++ FTP/FTP.c
@@ -14,11 +14,18 @@ int get_response(char* buffer, int sockfd, int print){
   return bytes_rcvd;
 }
 
-
 int send_command(const Command* command, int sockfd){
   const size_t cmd_length = CMD_LEN + strlen(command->arg);
   char cmd[cmd_length];
   sprintf(cmd, "%s %s\r\n", command->cmd, command->arg);
+  int bytes_sent = send(sockfd, cmd, cmd_length, 0);
+  return bytes_sent;
+}
+
+int send_response(const char* status, const char* msg, int sockfd){
+  const size_t cmd_length = strlen(status) + strlen(msg) + 1;
+  char cmd[cmd_length];
+  sprintf(cmd, "%s %s\r\n", status, msg);
   int bytes_sent = send(sockfd, cmd, cmd_length, 0);
   return bytes_sent;
 }
@@ -33,6 +40,7 @@ int read_command(Command* command, FILE* fp){
   strcpy(command->cmd, token);
   
   if((token = strtok(NULL, " "))!= NULL){
+    token = strtok(token, "\n");
     if(strlen(token) > ARG_LEN)
       token[ARG_LEN] = 0;
     strcpy(command->arg, token);
@@ -42,7 +50,7 @@ int read_command(Command* command, FILE* fp){
 }
 
 int get_command(Command* command, int sockfd, int print){
-  char buffer[MSG_LEN];
+  char buffer[MSG_LEN], *token;
   int bytes_rcvd = recv(sockfd, buffer, sizeof(buffer), 0);
   
   if(bytes_rcvd > 0){
@@ -50,15 +58,64 @@ int get_command(Command* command, int sockfd, int print){
     
     if(print)
       printf("%s\n", buffer);
+
+    token = strtok(buffer, " ");
+    if(strlen(token) > CMD_LEN)
+      token[CMD_LEN] = 0;
+    strcpy(command->cmd, token);
     
-    memcpy(command->cmd, buffer, CMD_LEN - 1);
-    command->cmd[CMD_LEN] = 0;
-    
-    memcpy(command->arg, buffer + CMD_LEN - 2, ARG_LEN);
-    command->arg[ARG_LEN] = 0;
+    if((token = strtok(NULL, " "))!= NULL){
+      if(strlen(token) > ARG_LEN)
+	token[ARG_LEN] = 0;
+      strcpy(command->arg, token);
+    }
   }
   
   return bytes_rcvd;
 }
 
+int handle_login(int sockfd){
+  Command c;
+  
+  /* Get user name (or die trying) */
+  get_command(&c, sockfd, 1);
+  while((strcmp(c.cmd, "USER") != 0)){
+    send_response("332", "Need account for login", sockfd);
+    get_command(&c, sockfd, 1);
+  }
+  
+  printf("READ USERNAME: %s\n", c.arg);
+  printf("In hex:\n");
+  print_hex(c.arg, strlen(c.arg));
+  
+  for(size_t i = 0; i < NUM_USERS + 1; ++i){
+    if(i == NUM_USERS){
+      send_response("430", "Invalid User name", sockfd);
+      return 1;
+    }
+    if(strcmp(USERS[i], c.arg) == 0){
+      send_response("331", "User name okay, need password", sockfd);
+      break;
+    }
+  }
 
+  /* Get password (WARNING: FTP TRANSMITS PASSWORDS IN PLAINTEXT) */
+  get_command(&c, sockfd, 1);
+  while((strcmp(c.cmd, "PASS") != 0)){
+    send_response("331", "User name okay, need password", sockfd);
+    get_command(&c, sockfd, 1);
+  }
+  
+  for(size_t i = 0; i < NUM_USERS + 1; ++i){
+    if(i == NUM_USERS){
+      send_response("430", "Invalid Password", sockfd);
+      return 1;
+    }
+    if(strcmp(PASSWORDS[i], c.arg) == 0){
+      send_response("230", "User logged in, proceed.", sockfd);
+      break;
+    }
+  }
+
+  return 0; 
+}
--- FTP/FTP.h
+++ FTP/FTP.h
@@ -5,10 +5,52 @@
 #include "../Stream/Stream.h"
 
 #define CMD_LEN 5
-#define ARG_LEN BUF
+#define ARG_LEN 507
 #define MSG_LEN CMD_LEN + ARG_LEN
+#define NUM_COMMANDS 14
+#define NUM_USERS 5
 
-//static const char* welcome =  "-=(<*>)=-.:. (( Welcome to The FTP server)) .:.-=(<*>)=-";
+#define PASSWORD password123
+
+static const char* USERS[NUM_USERS] = {"Euclid", "Newton", "Gauss", "Euler", "Hilbert"};
+static const char* PASSWORDS[NUM_USERS] = {"geometry", "calculus", "electrostatics",
+				    "konigsberg", "spaces"};
+
+static const char* welcome_message = "-=(<*>)=-.:. (( Welcome to The FTP server)) .:.-=(<*>)=-";
+
+static const char* COMMAND_STRING[14] = {
+  "ABOR", /* Abort previous FTP command */
+  "LIST", /* List file and directories */
+  "DELE", /* Delete a file */
+  "RMD",  /* Remove a directory */
+  "MKD",  /* Create a directory */
+  "PWD",  /* Print working directory */
+  "PASS", /* Send password (NOTE: THE FTP USES PLAINTEXT PASSWORDS)*/
+  "PORT", /* Request open port on specific IP addr/Port No */
+  "QUIT", /* Log off of server */
+  "RETR", /* Retrieve a file */
+  "STOR", /* Send or put a file */
+  "SYST", /* Identify system type */
+  "TYPE", /* Specify type (A for ASCII, I for binary) */
+  "USER"  /* Send Username */
+};
+
+typedef enum Command_Enum{
+  ABOR, 
+  LIST,
+  DELE,
+  RMD,
+  MKD,
+  PWD,
+  PASS,
+  PORT,
+  QUIT,
+  RETR,
+  STOR,
+  SYST,
+  TYPE,
+  USER
+}COMMAND_ENUM;
 
 typedef struct command{
   char cmd[CMD_LEN];
@@ -36,6 +78,16 @@ int get_response(char* buffer, int sockfd, int print);
  */
 int send_command(const Command* command, int sockfd);
 
+/*
+  Send a response from the server containing a status code followed by a message
+  
+  @param status  : Status code (see STATUS)
+  @param msg     : Message to accompany the status code
+  @param sockfd  : The socket file descriptor associated with the recipient.
+  @return int    : The number of bytes sent to the server, or a int < 0 
+                   if there occured an error.
+ */
+int send_response(const char* status, const char* msg, int sockfd);
 
 /*
   Read and convert an FTP command from a file (usually stdin)
@@ -47,7 +99,7 @@ int send_command(const Command* command, int sockfd);
 int read_command(Command* command, FILE* fp);
 
 /*
-  Send a valid FTP command to the server.
+  Get a valid FTP command to the server.
   
   @param command : A nonempty command struct.
   @param sockfd  : The socket file descriptor associated with the server.
@@ -55,7 +107,17 @@ int read_command(Command* command, FILE* fp);
   @return int    : The number of bytes sent to the server, or a int < 0 
                    if there occured an error.
  */
+
 int get_command(Command* command, int sockfd, int print);
+
+/*
+  Handle user login to the server
+  
+  @param sockfd  : The socket file descriptor associated with the server.
+  @return int    : 0 if user logged in, nonzero otherwise.
+
+ */
+int handle_login(int sockfd);
 
 /* Server Functions for handling each command each have the format...
    @param arg    : The null-terminated paramater of the command, usually
@@ -67,5 +129,5 @@ int get_command(Command* command, int sockfd, int print);
 
 
 int handle_pwd(const char* arg, int sockfd);
-
+int handle_pass(const char* arg, int sockfd);
 #endif
